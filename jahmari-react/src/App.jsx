c92991f537efbc4a5f3a4f29aecfce442a992b27
jahmarimaxwell <jahmari.maxwell@gmail.com>
2024-08-16 16:18:36 -0400

    got the change language functionality to work, must complete short answers

--- jahmari-react/src/App.jsx
+++ jahmari-react/src/App.jsx
@@ -1,52 +1,69 @@
 import { useState } from 'react'
 import './App.css'
-/// Components are capitalized
+///// Components are capitalized
+
 const SizeButtons = () => {
+  const [fontSize, setFontSize] = useState(24);
   return  (
    <>
-  <button>Increase Font</button>
-  <button>Decrease Font</button>
+  <button onClick={increaseFontSize}>Increase Font</button>
+  <button onClick={decreaseFontSize}>Decrease Font</button>
    </>
   )
 }
+const increaseFontSize = () => {
+  setFontSize(fontSize + 2);
+};
 
-const Greeting = () => {
-  return <h1>Good Morning</h1>
-}
+const decreaseFontSize = () => {
+  setFontSize(fontSize - 2);
+};
+
 
 const LanguageButtons = () => {
-  const [greeting, setGreeting] = useState(0);
+  const [language, setLanguage] = useState('English');
+
+  const translations = {
+    English: "Good Morning",
+    Spanish: "Buenos Días",
+    "Haitian Creole": "Bonjou",
+    Portuguese: "Bom Dia",
+    Japanese: "Ohayō",
+  }
+
+  const Greeting = () => {
+    return <h1>{translations[language]}</h1>
+  }
+
+  const changeLanguage = (language) => {
+    setLanguage(language);
+  };
+
   return (
     <>
-    <button class="greeting" onClick={HandleButtons}>English</button>
-    <button class="greeting" onClick={HandleButtons}>Haitian Creole</button>
-    <button class="greeting" onClick={HandleButtons}>Portguese</button>
-    <button class="greeting" onClick={HandleButtons}>Spanish</button>
-    <button class="greeting" onClick={HandleButtons}>Japanese</button>
-    </>
+      <Greeting />
+      <button onClick={() => changeLanguage("English")}>English</button>
+      <button onClick={() => changeLanguage("Spanish")}>Haitian Creole</button>
+      <button onClick={() => changeLanguage("Haitian Creole")}>Portguese</button>
+      <button onClick={() => changeLanguage("Portuguese")}>Spanish</button>
+      <button onClick={() => changeLanguage("Japanese")}>Japanese</button>
+   </>
+    /*
+    {Object.keys(translations).map((language) => (
+        <button key={language} onClick={() => changeLanguage(language)}>
+          {language}
+        </button>
+    ))}
+    */
   )
 }
-const HandleButtons = () => {
-  setGreeting(translations[language])
-}
-
 
 function App() {
-
-    const translations = {
-      English: "Good Morning",
-      Spanish: "Buenos Días",
-      "Haitian Creole": "Bonjou",
-      Portuguese: "Bom Dia",
-      Japense: "Ohayō",
-    };
-
     return (
       <>
-      <SizeButtons />
-      <Greeting />
-      <LanguageButtons />
-    </>
+        <SizeButtons />
+        <LanguageButtons />
+      </>
   )
 }
 
